276b890cb5112d181c5a1642b108dafc11f223a6
Marcela <marcelavi.sul@gmail.com>
2024-10-28 20:17:55 -0300

    Tela Principal pt.1

---
+++ app/src/main/java/com/example/ataverna/Rankers.java
@@ -0,0 +1,44 @@
+package com.example.ataverna;
+
+public class Rankers {
+
+    private String nome;
+    private String comentario;
+    private String album;
+    private String nota;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String setNome(String nome) {
+        return this.nome = nome;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public String setComentario(String comentario) {
+        return this.comentario = comentario;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public String setAlbum(String album) {
+        return this.album = album;
+    }
+
+    public String getNota() {
+        return nota;
+    }
+
+    public String setNota(String nota) {
+        return this.nota = nota;
+    }
+
+
+
+}
--- app/src/main/java/com/example/ataverna/TelaPrincipal.java
+++ app/src/main/java/com/example/ataverna/TelaPrincipal.java
@@ -3,16 +3,43 @@ package com.example.ataverna;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class TelaPrincipal extends BaseMainActivity {
+public class TelaPrincipal extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RankersAdapter adapter; // Adaptador personalizado para exibir os dados do modelo Rankers
+    private DatabaseReference mDatabase; // Referência do Firebase Realtime Database
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tela_principal);
+        setContentView(R.layout.tela_principal); // Defina o layout para esta activity
 
         ImageButton btnUsuario = findViewById(R.id.btnPerfilTelaPrincipal);
+
+        // Inicializa a referência ao nó do banco de dados do Firebase onde os dados do Rankers estão armazenados
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Rankers");
+
+        // Configura o RecyclerView
+        recyclerView = findViewById(R.id.rankers_recicla);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Configura as opções para o FirebaseRecyclerAdapter
+        FirebaseRecyclerOptions<Rankers> options = new FirebaseRecyclerOptions.Builder<Rankers>()
+                .setQuery(mDatabase.orderByChild("nome"), Rankers.class)
+                .build();
+
+        // Instancia o adaptador e o conecta ao RecyclerView
+        adapter = new RankersAdapter(options);
+        recyclerView.setAdapter(adapter);
 
         btnUsuario.setOnClickListener(View -> {
             Intent intent = new Intent(TelaPrincipal.this, TelaUsuario.class);
@@ -20,13 +47,17 @@ public class TelaPrincipal extends BaseMainActivity {
         });
     }
 
+    // Inicia a escuta do adaptador quando a Activity é iniciada
     @Override
-    protected void onViewCreated() {
-        
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
     }
 
-    private int dp2px(double dpValue) {
-      final float scale = getResources().getDisplayMetrics().density;
-      return (int) (dpValue * scale + 0.5f);
+    // Para a escuta do adaptador quando a Activity é pausada ou parada
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }

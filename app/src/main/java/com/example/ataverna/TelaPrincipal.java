4e51e89b2d9b1a20712fcee41b7757c13eed07cf
Marcela <marcelavi.sul@gmail.com>
2024-11-08 21:16:33 -0300

    tela principal

--- app/src/main/java/com/example/ataverna/TelaPrincipal.java
+++ app/src/main/java/com/example/ataverna/TelaPrincipal.java
@@ -1,7 +1,10 @@
 package com.example.ataverna;
 
+import static com.example.ataverna.TelaPesquisa.NEXT_SCREEN;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -11,6 +14,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ataverna.databinding.ActivityImageLoaderBinding;
+import com.example.ataverna.databinding.TelaPrincipalBinding;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -20,6 +26,11 @@ public class TelaPrincipal extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RankersAdapter adapter; // Adaptador personalizado para exibir os dados do modelo Rankers
     private DatabaseReference mDatabase; // Referência do Firebase Realtime Database
+    // Visualização Albuns
+    private RecyclerView recResult;
+    static TelaPrincipalBinding binding;
+    AlbumCapasAdapter adapterAlbuns;
+    DatabaseReference mbase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,19 +42,49 @@ public class TelaPrincipal extends AppCompatActivity {
 
         setContentView(R.layout.tela_principal); // Defina o layout para esta activity
 
+        binding = TelaPrincipalBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ImageButton btnPesquisa = findViewById(R.id.btnToPesquisa);
         ImageButton btnPerfilTelaPrincipal = findViewById(R.id.btnPerfilTelaPrincipal);
+
+        // Visualização dos álbuns
+        mbase = FirebaseDatabase.getInstance().getReference("Album");
+        recResult = findViewById(R.id.rvAlbuns);
 
         // Inicializa a referência ao nó do banco de dados do Firebase onde os dados do Rankers estão armazenados
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Rankers");
 
-        // Configura o RecyclerView
+        // Configura o RecyclerView (Albuns)
+        recResult.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        // Query
+        FirebaseRecyclerOptions<Album> optionsAlbuns
+                = new FirebaseRecyclerOptions.Builder<Album>()
+                .setQuery(mbase.orderByChild("reviews").limitToFirst(10), Album.class)
+                .build();
+
+        // Inicializa o Adapter
+        adapterAlbuns = new AlbumCapasAdapter(optionsAlbuns);
+        // Conecta o Adapter com o Recycler
+        recResult.setAdapter(adapterAlbuns);
+
+        adapterAlbuns.setOnClickListener(new AlbumCapasAdapter.OnClickListener() {
+            @Override
+            public void onClick(int position, Album model) {
+                Intent intent = new Intent(TelaPrincipal.this, TelaRanking.class);
+                intent.putExtra(NEXT_SCREEN, model);
+                startActivity(intent);
+            }
+        });
+
+        // Configura o RecyclerView (Rankers)
         recyclerView = findViewById(R.id.rankers_recicla);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         // Configura as opções para o FirebaseRecyclerAdapter
         FirebaseRecyclerOptions<Rankers> options = new FirebaseRecyclerOptions.Builder<Rankers>()
-                .setQuery(mDatabase.orderByChild("hora"), Rankers.class)
+                .setQuery(mDatabase.orderByChild("hora").limitToFirst(15), Rankers.class)
                 .build();
 
         // Instancia o adaptador e o conecta ao RecyclerView
@@ -72,6 +113,7 @@ public class TelaPrincipal extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         adapter.startListening();
+        adapterAlbuns.startListening();
     }
 
     // Para a escuta do adaptador quando a Activity é pausada ou parada
@@ -79,5 +121,6 @@ public class TelaPrincipal extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         adapter.stopListening();
+        adapterAlbuns.stopListening();
     }
 }
